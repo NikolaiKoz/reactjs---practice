fc4aaa31a40dbb92fa49e29cd2373dbb9f379be1
Jose Luis Vallejos <joseluisvallejos1995@gmail.com>
2023-03-21 20:36:21 -0300

    feat: :sparkles: eslint fue instalado y configurado

--- analizando-vite/.eslintrc.cjs
+++ analizando-vite/.eslintrc.cjs
@@ -1,22 +1,26 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
+	env: {
+		browser: true,
+		es2021: true,
+	},
+  settings: {
+    react: {
+      version: "detect"
+    }
   },
-  extends: [
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
-  ],
-  rules: {
-    'no-unused-vars': 'error'
-  }
-}
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'standard',
+		'eslint-config-prettier',
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		'no-unused-vars': 'error',
+	},
+};
