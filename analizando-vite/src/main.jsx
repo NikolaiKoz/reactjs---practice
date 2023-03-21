edab4d7637c5401b785b333d5be8e08aefc2753e
Jose Luis Vallejos <joseluisvallejos1995@gmail.com>
2023-03-21 10:55:12 -0300

    feat: :sparkles: Estudiando como funciona vite

---
+++ analizando-vite/src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDom from 'react-dom';
+import './index.css';
+
+const  app = <h1>Desarrollo util</h1>;
+const container = document.getElementById('root');
+
+ReactDom.render(app, container);
