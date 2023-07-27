9e9a0bc80dfe0f21802e92eafee63da7d873c865
Jose Luis Vallejos <joseluisvallejos1995@gmail.com>
2023-07-27 19:13:37 -0300

    feat: :sparkles: First example DONE.

--- useCallback/src/components/App.jsx
+++ useCallback/src/components/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  const doble = () => {
+    return count * 2;
+  };
+
+  const callBackFHook = useCallback(doble, [count]);
+  console.log( "callBack: \n" + callBackFHook);
+
+  const memoHook = useMemo(doble, [count]);
+  console.log( "memo: \n" + memoHook);
 
   return (
     <div>
       <button onClick={() => setCount(count + 1)}>Incrementar</button>
       <p>{count}</p>
+      <p>{doble()}</p>
     </div>
   );
 }
