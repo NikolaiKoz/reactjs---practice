43c30677e2d2f5ee32def2296cfd8dd2b0abae7e
Jose Luis Vallejos <joseluisvallejos1995@gmail.com>
2023-07-29 14:43:21 -0300

    refactor: :construction: useState WIp.

--- useState/src/components/App.jsx
+++ useState/src/components/App.jsx
@@ -1,21 +1,26 @@
-import { useState } from "react"
-
+import { useState } from "react";
 
 function App() {
-
   const [state, setState] = useState({
     mensaje: "Hola mundo",
-    numero: 3
-  })
+    numero: 3,
+  });
 
+  const handleClick = () => {
+    setState({
+      mensaje: "Adios",
+      numero: state.numero + 1,
+    })
+  };
 
   return (
     <div>
-      <h2>useState Hhook example</h2>
+      <h2>useState hook example</h2>
       <p>{state.mensaje}</p>
       <p>{state.numero}</p>
+      <button onClick={handleClick}>Cambiar estado</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
