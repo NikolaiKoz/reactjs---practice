5b57ed0f650b057842ff65a5d9ca472eb5b47a41
Vallejos Jose Luis <98178495+NikolaiKoz@users.noreply.github.com>
2024-02-01 15:19:58 -0300

    feat: :construction: useState

--- analizando-vite/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
-import './styles/css/index.css';
-import './styles/scss/resetPRO.scss';
-
-const container = document.getElementById('root');
-
-ReactDOM.createRoot(container).render(<App />);
