a0f4423d7e0b0da0e9ec411d778584ee8eec4797
Vallejos Jose Luis <98178495+NikolaiKoz@users.noreply.github.com>
2024-02-01 20:13:59 -0300

    fix: :zap: useState Donde.

--- hooks/basicHooks/02-useEffect/src/App.jsx
+++ hooks/basicHooks/02-useEffect/src/App.jsx
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
-	return <div>App</div>;
+	console.log('Cargando app')
+	const [contador, setContador] = useState(0);
+
+	useEffect(() => {
+		console.log(contador);
+		document.title = contador;
+	}, [contador]);
+	return (
+		<div>
+			<button
+				onClick={() => {
+					console.log('Cargando btn');
+					setContador(contador + 1);
+				}}
+			>
+				+1
+			</button>
+		</div>
+	);
 }
